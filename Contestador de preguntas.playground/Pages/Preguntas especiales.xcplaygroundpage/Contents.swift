43a867ca3be1857b41eb52e08531a624091f443f
MiguelRuizHdz <miguel.ruizhz@uanl.edu.mx>
2022-09-27 11:26:10 -0500

    Preguntas especiales

--- Contestador de preguntas.playground/Pages/Preguntas especiales.xcplaygroundpage/Contents.swift
+++ Contestador de preguntas.playground/Pages/Preguntas especiales.xcplaygroundpage/Contents.swift
@@ -7,15 +7,16 @@ func responseTo(question: String) -> String {
     let lowerQuestion = question.lowercased()
     print(lowerQuestion)
 
-    if lowerQuestion.hasPrefix("dónde") {
-        return ""¡Al norte!”
-    } else if lowerQuestion == "¿dónde están las galletas?" {
+    if lowerQuestion == "¿dónde están las galletas?" || lowerQuestion == "where are the cookies?" {
         return "En el frasco de galletas."
+    } else if lowerQuestion.hasPrefix("dónde") || lowerQuestion.hasPrefix("where")  {
+        return "¡Al norte!"
     } else {
         return "Eso depende"
     }
 }
 responseTo(question: "¿Dónde están las galletas?")
+responseTo(question: "Where are the cookies?")
 /*:
  - callout(Ejercicio): La función anterior es incorrecta. La primera instrucción condicional `if` determina si la pregunta comienza con “where”, lo cual es cierto. Esto significa que las instrucciones siguientes no se evalúan. Corrige la función anterior para obtener la respuesta “In the cookie jar!” (En el frasco de galletas) a la pregunta “Where are the cookies?” (¿Dónde están las galletas?).
 
