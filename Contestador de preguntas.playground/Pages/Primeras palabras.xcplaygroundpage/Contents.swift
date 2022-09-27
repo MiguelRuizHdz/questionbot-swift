90d0f90f09fc6d12586efcf8a2bc89ebf65a2a5b
MiguelRuizHdz <miguel.ruizhz@uanl.edu.mx>
2022-09-27 11:25:38 -0500

    Primeras palabras

--- Contestador de preguntas.playground/Pages/Primeras palabras.xcplaygroundpage/Contents.swift
+++ Contestador de preguntas.playground/Pages/Primeras palabras.xcplaygroundpage/Contents.swift
@@ -2,7 +2,7 @@
 ## Primeras palabras
  En este ejercicio, crearás una función que ofrezca respuestas diferentes según la primera palabra de la pregunta. El método `hasPrefix()` (tiene prefijo) evalúa si una cadena comienza con otra cadena:
  */
-“swift programming".hasPrefix("swift")
+"swift programming".hasPrefix("swift")
 "swift programming".hasPrefix("programming")
 //:  - callout(Ejercicio): Corrige la función y las pruebas que aparecen más abajo para proporcionar respuestas a distintas primeras palabras. Agrega respuestas para preguntas que comiencen con “who” (quién), “what” (qué), “why” (por qué) y “how” (cómo).
 func responseTo(question: String) -> String {
@@ -11,6 +11,14 @@ func responseTo(question: String) -> String {
         return "Por qué, hola"
     } else if question.hasPrefix("dónde") {
         return "¡Al norte!"
+    } else if question.hasPrefix("who") || question.hasPrefix("quién") {
+        return "¡Al norte!"
+    } else if question.hasPrefix("what") || question.hasPrefix("qué") {
+        return "¡Al norte!"
+    } else if question.hasPrefix("why") || question.hasPrefix("por qué") {
+        return "¡Al norte!"
+    } else if question.hasPrefix("how") || question.hasPrefix("cómo") {
+        return "¡Al norte!"
     } else {
         return "Eso depende"
     }
@@ -18,6 +26,10 @@ func responseTo(question: String) -> String {
 
 responseTo(question: "hola")
 responseTo(question: "¿a dónde debería irme de vacaciones?")
+responseTo(question: "¿cuál es la capital de Francia?")
+responseTo(question: "¿Where are you?")
+responseTo(question: "¿cuál es la capital de Francia?")
+responseTo(question: "¿cuál es la capital de Francia?")
 responseTo(question: "¿cuál es la capital de Francia?")
 //: Tal vez hayas notado que `where` (dónde) funciona, pero `Where` o `WHERE` no. En la página siguiente, aprenderás cómo manejar esta situación.
 
